83728e31ca0631a4c1c5cf8bd3ca1119e25fe416
RestlessRodent <ghostlydeath@remood.org>
2011-09-08 16:44:56 +0000

    Update headers to new ASCII logo

--- src/am_map.c
+++ src/am_map.c
@@ -1,18 +1,23 @@
 // -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-
-// -----------------------------------------------------------------------------
-// ########   ###### #####   #####  ######   ######  ######
-// ##     ##  ##     ##  ## ##  ## ##    ## ##    ## ##   ##
-// ##     ##  ##     ##   ###   ## ##    ## ##    ## ##    ##
-// ########   ####   ##    #    ## ##    ## ##    ## ##    ##
-// ##    ##   ##     ##         ## ##    ## ##    ## ##    ##
-// ##     ##  ##     ##         ## ##    ## ##    ## ##   ##
-// ##      ## ###### ##         ##  ######   ######  ######
-//                      http://remood.org/
-// -----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+//         :oCCCCOCoc.
+//     .cCO8OOOOOOOOO8Oo:
+//   .oOO8OOOOOOOOOOOOOOOCc
+//  cO8888:         .:oOOOOC.                                                TM
+// :888888:   :CCCc   .oOOOOC.     ###      ###                    #########
+// C888888:   .ooo:   .C########   #####  #####  ######    ######  ##########
+// O888888:         .oO###    ###  #####  ##### ########  ######## ####    ###
+// C888888:   :8O.   .C##########  ### #### ### ##    ##  ##    ## ####    ###
+// :8@@@@8:   :888c   o###         ### #### ### ########  ######## ##########
+//  :8@@@@C   C@@@@   oo########   ###  ##  ###  ######    ######  #########
+//    cO@@@@@@@@@@@@@@@@@Oc0
+//      :oO8@@@@@@@@@@Oo.
+//         .oCOOOOOCc.                                      http://remood.org/
+// ----------------------------------------------------------------------------
 // Copyright (C) 1993-1996 by id Software, Inc.
 // Copyright (C) 1998-2000 by DooM Legacy Team.
 // Copyright (C) 2008-2011 GhostlyDeath (ghostlydeath@gmail.com)
-// -----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 3
@@ -22,7 +27,7 @@
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// -----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 // DESCRIPTION: the automap code
 
 /* Includes */
