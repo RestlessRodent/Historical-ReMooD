c3f2e87e9a98f8c6fc51553567b67ce366968cb1
RestlessRodent <ghostlydeath@remood.org>
2012-05-31 23:42:58 +0000

    When early booting, don't draw past the screen.

--- src/console.c
+++ src/console.c
@@ -1768,7 +1768,7 @@ void CONL_EarlyBootTic(const char* const a_Message, const bool_t a_DoTic)
 	
 		/* Draw Message Text */
 		yB = 160;	// Base near the bottom somewhere
-		for (x = 0, c = EarlyBuf; *c; c++, x += 8)
+		for (x = 0, c = EarlyBuf; *c, x < 320 - 9; c++, x += 8)
 		{
 			// Get better character
 			Char = toupper(*c) - '!';
