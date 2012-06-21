5eaad237f513cad0c7416187adbd9d01a8c85c8b
RestlessRodent <ghostlydeath@remood.org>
2012-06-21 11:14:41 +0000

    Minor Changes

--- src/p_enemy.c
+++ src/p_enemy.c
@@ -409,6 +409,7 @@ static void P_NewChaseDir(mobj_t* actor)
 		if (actor->movedir != turnaround && P_TryWalk(actor))
 			return;
 	}
+	
 	// try other directions
 	if (P_Random() > 200 || abs(deltay) > abs(deltax))
 	{
@@ -439,6 +440,7 @@ static void P_NewChaseDir(mobj_t* actor)
 		if (P_TryWalk(actor))
 			return;
 	}
+	
 	// there is no direct path to the player,
 	// so pick another direction.
 	if (olddir != DI_NODIR)
@@ -584,66 +586,6 @@ static bool_t P_LookForPlayers(mobj_t* actor, bool_t allaround)
 	BestMo = NULL;
 	BestDist = 20000 << FRACBITS;
 	
-#if 0
-	// Look through adjacent sectors
-	sector = actor->subsector->sector;
-	for (c = 0; c < sector->NumAdj && !BestMo; c++)
-		// Thing chains
-		for (mo = sector->Adj[c]->thinglist; mo && !BestMo; mo = mo->snext)
-		{
-			// Ourself?
-			if (actor == mo)
-				continue;
-				
-			// Not Shootable?
-			if (!(mo->flags & MF_SHOOTABLE))
-				continue;
-		
-			// Dead?
-			if ((mo->health <= 0) || (mo->flags & MF_CORPSE))
-				continue;
-			
-			// A player? and cannot target them?
-			if ((mo->RXFlags[0] & MFREXA_ISPLAYEROBJECT) && P_EXGSGetValue(PEXGSBID_FUNNOTARGETPLAYER))
-				continue;
-			
-			// On the same team?
-			if (P_MobjOnSameTeam(actor, mo))
-				if (!(P_EXGSGetValue(PEXGSBID_FUNMONSTERFFA)))
-					continue;
-					
-			// Is it in view?
-			if (!P_CheckSight(actor, mo))
-				continue;
-			
-			// Look all around?
-			if (!allaround)
-			{
-				an = R_PointToAngle2(actor->x, actor->y, mo->x, mo->y) - actor->angle;
-
-				if (an > ANG90 && an < ANG270)
-				{
-					dist = P_AproxDistance(mo->x - actor->x, mo->y - actor->y);
-				
-					// if real close, react anyway
-					if (dist > MELEERANGE)
-						continue;	// behind back
-				}
-			}
-		
-			// Distance Check
-			dist = P_AproxDistance(actor->x - mo->x, actor->y - mo->y);
-			if (dist < BestDist)
-			{
-				BestDist = dist;
-				BestMo = mo;
-			
-				// Distance is REALLY close? Then target that thing
-				if (BestDist < (128 << FRACBITS))
-					break;
-			}
-		}
-#else
 	// Look through thinkers
 	for (currentthinker = thinkercap.next; currentthinker != &thinkercap; currentthinker = currentthinker->next)
 	{
@@ -706,7 +648,6 @@ static bool_t P_LookForPlayers(mobj_t* actor, bool_t allaround)
 				break;
 		}
 	}
-#endif
 	
 	// Found best?
 	if (BestMo)
@@ -831,10 +772,8 @@ void A_Chase(mobj_t* actor, player_t* player, pspdef_t* psp, const INFO_StateArg
 	// modify target threshold
 	if  (actor->threshold)
 	{
-		// Use Heretic Threshold Logic
-		if (P_EXGSGetValue(PEXGSBID_HEREMONSTERTHRESH))
-			actor->threshold--;
-		else if (!actor->player && (!actor->target || actor->target->health <= 0 || (actor->target->flags & MF_CORPSE)))
+		// No target or target is dead
+		if (!P_EXGSGetValue(PEXGSBID_HEREMONSTERTHRESH) && !actor->player && (!actor->target || actor->target->health <= 0 || (actor->target->flags & MF_CORPSE)))
 		{
 			actor->threshold = 0;
 		
@@ -844,6 +783,7 @@ void A_Chase(mobj_t* actor, player_t* player, pspdef_t* psp, const INFO_StateArg
 		}
 		
 		// There is a target, it is alive
+		// Or playing Heretic
 		else
 			actor->threshold--;
 	}
