1dc66e7cfdaf7bddd15a0d5cc9e08e4efebc61bf
RestlessRodent <ghostlydeath@remood.org>
2012-06-25 01:51:31 +0000

    Switch to C++ emacs modeline (for compatible editors)

--- src/t_dsvm.cxx
+++ src/t_dsvm.cxx
@@ -1,4 +1,4 @@
-	// -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-
+	// -*- Mode: C++; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-
 // -----------------------------------------------------------------------------
 //         :oCCCCOCoc.
 //     .cCO8OOOOOOOOO8Oo:
