c4294ac700eb13128d53a0bb790a022113abcb0d
RestlessRodent <ghostlydeath@remood.org>
2012-12-09 14:13:45 +0000

    Add libogc directory for NGC/Wii support.

---
+++ src/libogc/i_main.c
@@ -0,0 +1,57 @@
+// -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-
+// ----------------------------------------------------------------------------
+//         :oCCCCOCoc.
+//     .cCO8OOOOOOOOO8Oo:
+//   .oOO8OOOOOOOOOOOOOOOCc
+//  cO8888:         .:oOOOOC.                                                TM
+// :888888:   :CCCc   .oOOOOC.     ###      ###                    #########
+// C888888:   .ooo:   .C########   #####  #####  ######    ######  ##########
+// O888888:         .oO###    ###  #####  ##### ########  ######## ####    ###
+// C888888:   :8O.   .C##########  ### #### ### ##    ##  ##    ## ####    ###
+// :8@@@@8:   :888c   o###         ### #### ### ########  ######## ##########
+//  :8@@@@C   C@@@@   oo########   ###  ##  ###  ######    ######  #########
+//    cO@@@@@@@@@@@@@@@@@Oc0
+//      :oO8@@@@@@@@@@Oo.
+//         .oCOOOOOCc.                                      http://remood.org/
+// ----------------------------------------------------------------------------
+// Copyright (C) 2011-2013 GhostlyDeath <ghostlydeath@remood.org>
+//                                      <ghostlydeath@gmail.com>
+// ----------------------------------------------------------------------------
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// ----------------------------------------------------------------------------
+// DESCRIPTION:
+//      Main program, simply calls D_DoomMain high level loop.
+
+/***************
+*** INCLUDES ***
+***************/
+
+#include "doomdef.h"
+#include "m_argv.h"
+#include "d_main.h"
+
+/****************
+*** FUNCTIONS ***
+****************/
+
+/* main() -- Main entry point */
+int main(int argc, char** argv)
+{
+	/* Set command line */
+	I_CommonCommandLine(&argc, &argv, NULL);
+	
+	/* Run the game */
+	D_DoomMain();
+	D_DoomLoop();
+	
+	/* Success! */
+	return EXIT_SUCCESS;
+}
