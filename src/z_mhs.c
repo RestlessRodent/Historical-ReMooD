473bc8c8f3641fcb44a199e2f6397dc6dce4f0da
RestlessRodent <ghostlydeath@remood.org>
2013-08-11 14:59:03 +0000

    Add memory usage table command "memusage"

--- src/z_mhs.c
+++ src/z_mhs.c
@@ -36,6 +36,7 @@
 #include "z_zone.h"
 #include "i_system.h"
 #include "i_util.h"
+#include "console.h"
 
 /****************
 *** CONSTANTS ***
@@ -75,6 +76,7 @@ typedef struct Z_MHCMiniBlock_s
 	
 	struct Z_MHCInfo_t* Info;					// First in info
 	uint32_t AllocSize;							// Allocation size
+	uint32_t FreeSize;							// Free space inside
 	
 	uint32_t End;								// End of mini block
 } Z_MHCMiniBlock_t;
@@ -88,11 +90,14 @@ typedef struct Z_MHCInfo_s
 	Z_MemoryTag_t Tag;							// Allocation Tag
 	void** RefPtr;								// Reference
 	
+	Z_MHCMiniBlock_t* MiniBlock;				// Mini block if inside one
 	struct Z_MHCInfo_s* Prev;					// Previous
 	struct Z_MHCInfo_s* Next;					// Next
-	Z_MHCMiniBlock_t* MiniBlock;				// Mini block if inside one
-	struct Z_MHCInfo_s* MiniPrev;				// Previous mini block neighbor
-	struct Z_MHCInfo_s* MiniNext;				// Next " " " 
+	
+#if defined(_DEBUG)
+	const char* File;							// File used by
+	int Line;									// Line location
+#endif
 	
 	uint32_t End;								// Marker End
 } Z_MHCInfo_t;
@@ -101,6 +106,7 @@ typedef struct Z_MHCInfo_s
 *** LOCALS ***
 *************/
 
+static Z_MHCMiniBlock_t* l_ZMinis[NUMZTAGS + 1];
 static Z_MHCInfo_t* l_ZChains[NUMZTAGS + 1];
 static uint64_t l_ZAllocSize[2] = {0, 0};
 
@@ -113,6 +119,7 @@ void Z_MHC_Init(void)
 {
 	/* Clear chain list */
 	memset(l_ZChains, 0, sizeof(l_ZChains));
+	memset(l_ZMinis, 0, sizeof(l_ZMinis));
 }
 
 /* Z_MHC_MallocExWrappee() -- Allocates space */
@@ -166,6 +173,12 @@ void* Z_MHC_MallocExWrappee(const size_t a_Size, const Z_MemoryTag_t a_Tag, void
 			l_ZChains[AllocBlock->Tag]->Prev = AllocBlock;
 			l_ZChains[AllocBlock->Tag] = AllocBlock;
 		}
+		
+		// File and line
+#if defined(_DEBUG)
+		AllocBlock->File = File;
+		AllocBlock->Line = Line;
+#endif
 		
 		// Return offset of
 		return (void*)((uintptr_t)AllocBlock + ZBLOCKBASE);
@@ -314,7 +327,180 @@ Z_MemoryTag_t Z_MHC_ChangeTagWrappee(void* const Ptr, const Z_MemoryTag_t NewTag
 	return OldTag;
 }
 
+#if defined(_DEBUG)
+/* Z_MHZDbgRef_t -- Debug reference */
+typedef struct Z_MHZDbgRef_s
+{
+	uint32_t Hash;								// Hash or zero
+	const char* File;							// Filename
+	uint32_t Blocks;							// Owned blocks
+	uint32_t Bytes;								// Bytes allocated
+	uint32_t Cache;								// Cached bytes used
+	uint32_t EstWaste;							// Estimated wasted space
+} Z_MHZDbgRef_t;
+#endif
+
+/* Z_MHC_MemUsage() -- Memory usage */
+static int Z_MHC_MemUsage(const uint32_t a_ArgC, const char** const a_ArgV)
+{
+#if defined(_DEBUG)
+#define MAXCOUNT 256
+	int32_t i, j, k;
+	Z_MHCInfo_t* Alloc;
+	uint32_t HashF;
+	Z_MHZDbgRef_t Refs[MAXCOUNT];
+	Z_MHZDbgRef_t* Ref, Temp;
+	const char* FName;
+	
+	uint32_t Bu[4];
+	char Bc[4];
+#endif
+	
+	/* Totals */
+	CONL_PrintF("Total: %u KiB\n", l_ZAllocSize[1] / 1024);
+	CONL_PrintF("Avail: %u KiB\n", l_ZAllocSize[0] / 1024);
+	CONL_PrintF("Waste: %u KiB\n", (l_ZAllocSize[1] - l_ZAllocSize[0]) / 1024);
+
+	/* Deep memory debug */
+#if defined(_DEBUG)
+	// Clear references
+	memset(Refs, 0, sizeof(Refs));	
+	
+	// Need to go through each tag, being unique
+	for (i = 0; i < NUMZTAGS; i++)
+		for (Alloc = l_ZChains[i]; Alloc; Alloc = Alloc->Next)
+		{
+			// Find reference from file
+			if (Alloc->File)
+			{
+				// Hashfile name
+				HashF = Z_Hash(Alloc->File);
+			
+				// See if it exists in refs already
+				for (j = 1; j < MAXCOUNT; j++)
+					if ((Ref = &Refs[j])->Hash == HashF)
+						break;
+			
+				// Does not
+				if (j >= MAXCOUNT)
+				{
+					// Try to find a free spot
+					for (j = 1; j < MAXCOUNT; j++)
+						if (!Refs[j].Hash)
+						{
+							Ref = &Refs[j];
+							Ref->Hash = HashF;
+							Ref->File = Alloc->File;
+							break;
+						}
+				
+					// Nothing
+					if (j >= MAXCOUNT)
+						continue;
+				}
+			}
+			
+			// Unknown (no file passed!?!?)
+			else
+				Ref = &Refs[0];
+			
+			// Increase reference totals
+			Ref->Blocks++;
+			
+			if (Alloc->Tag >= PU_PURGELEVEL)
+				Ref->Cache += Alloc->Size;
+			else
+				Ref->Bytes += Alloc->Size;
+			
+			// Add wastage of space
+			Ref->EstWaste += ZBLOCKBASE;
+		}
+	
+	/* Sort by total */
+	for (i = 1; i < MAXCOUNT; i++)
+	{
+		// Find highest
+		for (k = i, j = i; j < MAXCOUNT; j++)
+			if ((Refs[j].Bytes + Refs[j].Cache) > (Refs[k].Bytes + Refs[k].Cache))
+				k = j;
+		
+		// Can swap
+		if (k != i)
+		{
+			Temp = Refs[k];
+			Refs[k] = Refs[i];
+			Refs[i] = Temp;
+		}
+	}
+	
+	/* Print into on all references */
+	CONL_PrintF("%-13s: %-5s|%-5s?|%-5s?|%-5s?|%-5s?\n",
+			"File", "Blcks", "Used", "Cache", "Total", "Waste"
+		);
+	for (j = 0; j < MAXCOUNT; j++)
+	{
+		Ref = &Refs[j];
+		
+		if (!Ref->Hash && j != 0)
+			continue;
+		
+		// Get last characters of file
+		FName = (j == 0 ? "Unknown" : Ref->File);
+		
+		// If too long, reduce characters (so it fits better)
+		// And removes any useless prefix like src/ or C:\Long Directory\...
+		i = strlen(FName);
+		if (i >= 12)
+			FName += i - 12;
+			
+		// Shorten for Ks
+		Bu[0] = Ref->Bytes;
+		Bu[1] = Ref->Cache;
+		Bu[2] = Bu[0] + Bu[1];
+		Bu[3] = Ref->EstWaste;
+		
+		// Init letters to c
+		for (i = 0; i < 4; i++)
+			Bc[i] = 'B';
+		
+		// Go through, and lower
+		for (i = 0; i < 4; i++)
+			while (Bu[i] >= 10240)
+			{
+				// Decrease amount
+				Bu[i] /= 1024;
+				
+				// And increase the intensity
+				switch (Bc[i])
+				{
+					case 'B': Bc[i] = 'K'; break;
+					case 'K': Bc[i] = 'M'; break;
+					case 'M': Bc[i] = 'G'; break;
+					case 'G': Bc[i] = 'T'; break;
+					default: Bc[i] = 'P'; break;
+				}
+			}
+		
+		// A nice message
+		CONL_PrintF("%-13s: %5u|%5u%c|%5u%c|%5u%c|%5u%c\n",
+				FName,
+				Ref->Blocks,
+				Bu[0], Bc[0],
+				Bu[1], Bc[1],
+				Bu[2], Bc[2],
+				Bu[3], Bc[3]
+			);
+	}
+#undef MAXCOUNT
+#endif
+
+	/* Done */
+	return 0;
+}
+
+/* Z_MHC_RegisterCommands() -- Regsters memory manager commands and variables */
 void Z_MHC_RegisterCommands(void)
 {
+	CONL_AddCommand("memusage", Z_MHC_MemUsage);
 }
 
