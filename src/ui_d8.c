e97063885b6d7b529602803a564cf53c459d5fdf
RestlessRodent <ghostlydeath@remood.org>
2013-10-04 12:33:21 +0000

    8-bit Drawer

--- src/ui_d8.c
+++ src/ui_d8.c
@@ -47,6 +47,64 @@
 /* UI_d8_DrawImg() -- Draws image */
 void UI_d8_DrawImg(UI_BufferSpec_t* const a_Spec, UI_Img_t* const a_Img, const int32_t a_X, const int32_t a_Y)
 {
+	register int x1, y1, x2, y2;
+	register int y, ox, oy;
+	uint8_t* Dest, *Src;
+	
+	/* Check */
+	if (!a_Spec || !a_Img)
+		return;
+	
+	/* Coordinates outside buffer */
+	if (a_X >= a_Spec->w || a_Y >= a_Spec->h)
+		return;
+	
+	/* Calculate clipping coordinates */
+	x2 = a_X + a_Img->l[0];
+	y2 = a_Y + a_Img->l[1];
+	
+	if (x2 >= a_Spec->w)
+		x2 = a_Spec->w - 1;
+	if (y2 >= a_Spec->h)
+		y2 = a_Spec->h - 1;
+	
+	// Resulting draw end is zero length or negative
+	if (x2 <= a_X || y2 <= a_Y)
+		return;
+	
+	// Left bound offset?
+	if (a_X < 0)
+	{
+		x1 = 0;
+		ox = a_X;
+	}
+	else
+	{
+		x1 = a_X;
+		ox = 0;
+	}
+		
+	if (a_Y < 0)
+	{
+		y1 = 0;
+		oy = a_Y;
+	}
+	else
+	{
+		y1 = a_Y;
+		oy = 0;
+	}
+	
+	/* Draw loop */
+	Dest = ((uint8_t*)a_Spec->Data) + (uintptr_t)((y1 * a_Spec->p) + x1);
+	Src = ((uint8_t*)a_Img->Data) + (uintptr_t)((oy * a_Img->p) + ox);
+	for (y = y1; y < y2; y++)
+	{
+		memcpy(Dest, Src, (x2 - x1));
+		
+		Dest = (uint8_t*)((uintptr_t)Dest + a_Spec->pd);
+		Src = (uint8_t*)((uintptr_t)Src + a_Img->pd);
+	}
 }
 
 /* NOT IN DEDICATED SERVER */
