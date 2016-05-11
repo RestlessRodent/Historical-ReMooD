8890e57525fbb5e90c32b3386b126413b563a896
RestlessRodent <ghostlydeath@remood.org>
2016-05-11 22:44:28 +0000

    Automap colors.

---
+++ core/src/org/remood/remood/core/profile/automap/AutomapColors.java
@@ -0,0 +1,64 @@
+// -*- Mode: Java; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// ReMooD Doom Source Port <http://remood.org/>
+//   Copyright (C) 2005-2016 GhostlyDeath <ghostlydeath@remood.org>
+//     For more credits, see readme.mkd.
+// ---------------------------------------------------------------------------
+// ReMooD is under the GNU General Public License v3+, see license.mkd.
+// ---------------------------------------------------------------------------
+
+package org.remood.remood.core.profile.automap;
+
+/**
+ * The automap colors which are available.
+ *
+ * @since 2016/05/11
+ */
+public class AutomapColors
+{
+	/** Background Color. */
+	BACKGROUND,
+	
+	/** Your Color. */
+	YOUR_PLAYER,
+	
+	/** Thing Color. */
+	THING,
+	
+	/** Ally Thing Color. */
+	ALLY_THING,
+	
+	/** Enemy Thing Color. */
+	ENEMY_THING,
+	
+	/** Pickup Item. */
+	PICKUP,
+	
+	/** Wall. */
+	SOLID_WALL,
+	
+	/** Floor Step. */
+	FLOOR_STEP,
+	
+	/** Ceiling Step. */
+	CEIL_STEP,
+	
+	/** Trigger line. */
+	TRIGGER,
+	
+	/** Unmapped line. */
+	UNMAPPED,
+	
+	/** Blockmap grid color. */
+	GRID,
+	
+	/** Default Color. */
+	DEFAULT,
+	
+	/** Invisible double wall. */
+	INVISIBLE_WALL,
+	
+	/** End. */
+	;
+}
+
