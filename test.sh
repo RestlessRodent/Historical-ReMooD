7bed1274ed62d18658359dfa568fcff1e845e45f
RestlessRodent <ghostlydeath@remood.org>
2016-05-19 17:46:59 +0000

    Fixup the test script.

--- test.sh
+++ test.sh
@@ -7,57 +7,92 @@
 # ReMooD is under the GNU General Public License v3+, see license.mkd.
 # ---------------------------------------------------------------------------
 
-# Major java version
-__majorversions="7 8"
-
 # Architectures
-__architectures="$(uname -m) ppc powerpc i386 amd64 x86_64"
+__archs="$(uname -m) ppc powerpc i386 amd64 x86_64"
 
 # Available VMs
 __vms="server zero jamvm"
 
-# Find Java include header
-__findinclude()
+# Alternative name for architecture?
+__altname()
 {
-	for __jv in $__majorversions
+	case "$1" in
+		powerpc)
+			echo "ppc";
+			;;
+		
+		x86_64)
+			echo "amd64";
+			;;
+		
+		*)
+			echo "$1"
+		;;
+	esac
+}
+
+# Alternative architectures?
+__altarchs=$(for __a in $__archs; do __altname $__a; done)
+
+# Find base directory
+#/usr/lib/jvm/java-7-openjdk-powerpc/include/
+#/usr/lib/jvm/java-7-openjdk-powerpc/jre/lib/ppc/server/
+__findinc()
+{
+	for __i in $(seq 7 9)
 	do
-		for __ja in $__architectures
+		for __a in $__archs $__altarchs
 		do
-			__p=""
-			if [ -f "$__p/jni.h" ]
+			__try="/usr/lib/jvm/java-$__i-openjdk-$__a"
+			if [ -d "$__try" ]
 			then
-				echo "$__p"
-				exit 1
+				if [ -d "$__try/include" ]
+				then
+					echo "$__try/include"
+					exit 0
+				fi
+			fi
+		done
+	done
+}
+__findlib()
+{
+	for __i in $(seq 7 9)
+	do
+		for __a in $__archs $__altarchs
+		do
+			__try="/usr/lib/jvm/java-$__i-openjdk-$__a"
+			if [ -d "$__try" ]
+			then
+				for __b in $__archs $__altarchs
+				do
+					__tryb="$__try/jre/lib/$__b"
+					if [ -d "$__tryb" ]
+					then
+						for __v in $__vms
+						do
+							if [ -f "$__tryb/$__v/libjvm.so" ]
+							then
+								echo "$__tryb/$__v/"
+								exit 1
+							fi
+						done
+					fi
+				done
 			fi
 		done
 	done
 }
 
-# Find library directory
-__findlib()
-{
-	for __jv in $__majorversions
-	do
-		for __ja in $__architectures
-		do
-			for __vm in $__vms
-			do
-				__p=""
-				if [ -f "$__p/libjvm.so" ]
-				then
-					echo "$__p"
-					exit 1
-				fi
-			done
-		done
-	done
-}
-
 # Build and run it
-if make "JAVA_INCLUDE=/usr/lib/jvm/java-7-openjdk-powerpc/include/" \
-	"JAVA_LIB=/usr/lib/jvm/java-7-openjdk-powerpc/jre/lib/ppc/server/" DEBUG=1
+__ji="$(__findinc)"
+__jl="$(__findlib)"
+echo ">> INC: $__ji"
+echo ">> LIB: $__jl"
+if make "JAVA_INCLUDE=$__ji" \
+	"JAVA_LIB=$__jl" DEBUG=1
 then
-	LD_LIBRARY_PATH="/usr/lib/jvm/java-7-openjdk-powerpc/jre/lib/ppc/jamvm/:." \
+	LD_LIBRARY_PATH="$__jl:." \
 		gdb --args bin/remood-dbg -nomouse -devparm $*
 fi
 
