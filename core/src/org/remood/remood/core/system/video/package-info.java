4cfc75f44bd8ebe89bd9e3b9d4dcefe37f74d700
RestlessRodent <ghostlydeath@remood.org>
2016-05-19 11:53:06 +0000

    More base packages.

---
+++ core/src/org/remood/remood/core/system/video/package-info.java
@@ -0,0 +1,16 @@
+// -*- Mode: Java; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-
+// ----------------------------------------------------------------------------
+// ReMooD Doom Source Port <http://remood.org/>
+//   Copyright (C) 2005-2016 GhostlyDeath <ghostlydeath@remood.org>
+//     For more credits, see readme.mkd.
+// ----------------------------------------------------------------------------
+// ReMooD is under the GNU General Public License v3+, see license.mkd.
+// ----------------------------------------------------------------------------
+
+/**
+ * This contains the video interface.
+ *
+ * @since 2016/05/19
+ */
+package org.remood.remood.core.system.video;
+
