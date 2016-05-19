202228c5830ee2e4c23e0fdcfb1ce416b4871299
RestlessRodent <ghostlydeath@remood.org>
2016-05-19 11:54:21 +0000

    Base for device driver interface.

---
+++ core/src/org/remood/remood/core/system/DeviceDriver.java
@@ -0,0 +1,27 @@
+// -*- Mode: Java; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// ReMooD Doom Source Port <http://remood.org/>
+//   Copyright (C) 2005-2016 GhostlyDeath <ghostlydeath@remood.org>
+//     For more credits, see readme.mkd.
+// ---------------------------------------------------------------------------
+// ReMooD is under the GNU General Public License v3+, see license.mkd.
+// ---------------------------------------------------------------------------
+
+package org.remood.remood.core.system;
+
+/**
+ * This is the interface which acts as the base for all device drivers.
+ *
+ * @since 2016/05/19
+ */
+public interface class DeviceDriver
+{
+	/**
+	 * Returns the name of the device driver.
+	 *
+	 * @return The device driver name.
+	 * @since 2016/05/19
+	 */
+	public abstract String name();
+}
+
