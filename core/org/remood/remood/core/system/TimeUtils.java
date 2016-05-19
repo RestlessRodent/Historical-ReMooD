8817573e906eed2364991562cca3bc0220165920
RestlessRodent <ghostlydeath@remood.org>
2016-05-19 14:56:27 +0000

    Add timer methods.

--- core/org/remood/remood/core/system/TimeUtils.java
+++ core/org/remood/remood/core/system/TimeUtils.java
@@ -21,6 +21,10 @@ public final class TimeUtils
 	private static final long _BASE_TIME =
 		System.nanoTime();
 	
+	/** Nanoseconds per tick. */
+	private static final long _NS_PER_TIC =
+		28_571_428L;
+	
 	/**
 	 * Not used.
 	 *
@@ -28,6 +32,40 @@ public final class TimeUtils
 	 */
 	private TimeUtils()
 	{
+	}
+	
+	/**
+	 * Returns the number of nanoseconds that have passed.
+	 *
+	 * @return The number of nanoseconds that have passed since the base clock
+	 * was initialized.
+	 * @since 2016/05/19
+	 */
+	public static long baseTime()
+	{
+		return System.nanoTime() - _BASE_TIME;
+	}
+	
+	/**
+	 * Returns the number of milliseconds which have passed.
+	 *
+	 * @return The number of milliseconds that have passed.
+	 * @since 2016/05/19
+	 */
+	public static long getMilliseconds()
+	{
+		return baseTime() / 1_000_000L;
+	}
+	
+	/**
+	 * Returns the number of tics which have passed.
+	 *
+	 * @return The number of tics which have passed.
+	 * @since 2016/05/19
+	 */
+	public static long getTics()
+	{
+		return baseTime() / _NS_PER_TIC;
 	}
 }
 
