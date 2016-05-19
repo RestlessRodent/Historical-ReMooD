5864fb6c3159db0b22a90cc06c855b791c5d0f23
RestlessRodent <ghostlydeath@remood.org>
2016-05-19 11:47:24 +0000

    Base for the system package.

---
+++ core/src/org/remood/remood/core/system/package-info.java
@@ -0,0 +1,17 @@
+// -*- Mode: Java; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-
+// ----------------------------------------------------------------------------
+// ReMooD Doom Source Port <http://remood.org/>
+//   Copyright (C) 2005-2016 GhostlyDeath <ghostlydeath@remood.org>
+//     For more credits, see readme.mkd.
+// ----------------------------------------------------------------------------
+// ReMooD is under the GNU General Public License v3+, see license.mkd.
+// ----------------------------------------------------------------------------
+
+/**
+ * This package contains the system interfaces which are used to interact with
+ * the native host.
+ *
+ * @since 2016/05/19
+ */
+package org.remood.remood.core.system;
+
