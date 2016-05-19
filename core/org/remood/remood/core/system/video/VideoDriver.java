c0552c78e053220b4d9da60cca6d9138e4e603e3
RestlessRodent <ghostlydeath@remood.org>
2016-05-19 12:18:19 +0000

    Base for video driver interface.

---
+++ core/org/remood/remood/core/system/video/VideoDriver.java
@@ -0,0 +1,21 @@
+// -*- Mode: Java; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-
+// ---------------------------------------------------------------------------
+// ReMooD Doom Source Port <http://remood.org/>
+//   Copyright (C) 2005-2016 GhostlyDeath <ghostlydeath@remood.org>
+//     For more credits, see readme.mkd.
+// ---------------------------------------------------------------------------
+// ReMooD is under the GNU General Public License v3+, see license.mkd.
+// ---------------------------------------------------------------------------
+
+package org.remood.remood.core.system.video;
+
+/**
+ * This package contains the video driver interface which is implemented by
+ * libraries so that the user can actually see and interact with the game.
+ *
+ * @since 2016/05/19
+ */
+public interface VideoDriver
+{
+}
+
