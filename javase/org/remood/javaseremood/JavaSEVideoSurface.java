5450749547fd2c7674fca4d31e1ef566720721db
RestlessRodent <ghostlydeath@remood.org>
2016-07-17 13:02:38 +0000

    Setup of frame and framebuffer.

--- javase/org/remood/javaseremood/JavaSEVideoSurface.java
+++ javase/org/remood/javaseremood/JavaSEVideoSurface.java
@@ -9,11 +9,16 @@
 
 package org.remood.javaseremood;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import org.remood.remood.core.system.video.VideoDriver;
 import org.remood.remood.core.system.video.VideoException;
 import org.remood.remood.core.system.video.VideoSoftwareSurface;
 import org.remood.remood.core.system.video.VideoSurface;
-import javax.swing.JFrame;
 
 /**
  * This class provides a video surface which uses Swing.
@@ -23,6 +28,15 @@ import javax.swing.JFrame;
 public class JavaSEVideoSurface
 	extends VideoSoftwareSurface
 {
+	/** The display frame. */
+	private volatile JFrame _frame;
+	
+	/** The image backing the framebuffer. */
+	private volatile BufferedImage _image;
+	
+	/** The view pane. */
+	private volatile __Pane__ _pane;
+	
 	/**
 	 * Initializes the video surface.
 	 *
@@ -43,8 +57,59 @@ public class JavaSEVideoSurface
 	@Override
 	protected int[] createFramebuffer()
 	{
-		System.err.println("TODO -- Create framebuffer.");
-		return super.createFramebuffer();
+		// Setup frame
+		JFrame frame = new JFrame("ReMooD");
+		this._frame = frame;
+		
+		// Limit frame size to the resolution size
+		int w = this.width, h = this.height;
+		frame.setMinimumSize(new Dimension(w, h));
+		
+		// Setup image
+		BufferedImage image = new BufferedImage(
+			w, h, BufferedImage.TYPE_INT_RGB);
+		this._image = image;
+		
+		// Create panel
+		__Pane__ pane = new __Pane__();
+		this._pane = pane;
+		frame.add(pane);
+		
+		// Make it visible and center on the screen
+		frame.setVisible(true);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		
+		// Return raw pixel buffer to the framebuffer
+		return ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+	}
+	
+	/**
+	 * Panel used for drawing.
+	 *
+	 * @since 2016/07/17
+	 */
+	private class __Pane__
+		extends JPanel
+	{
+		/**
+		 * {@inheritDoc}
+		 * @since 2016/07/17
+		 */
+		@Override
+		public void paintComponent(Graphics __g)
+		{
+			// Draw parent components
+			super.paintComponent(__g);
+			
+			// Get video size
+			int w = JavaSEVideoSurface.this.width,
+				h = JavaSEVideoSurface.this.height;
+			
+			// Draw framebuffer
+			__g.drawImage(JavaSEVideoSurface.this._image.
+				getSubimage(0, 0, w, h), w, h, null);
+		}
 	}
 }
 
